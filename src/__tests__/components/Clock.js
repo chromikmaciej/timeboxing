4fed8ad34a0ea54343af70ee443e04711e252be2
m.chromik <chromik.maciej@gmail.com>
2020-02-23 22:08:36 +0100

    Test Clock component with ReactTestRenderer and Snapshots

---
+++ src/__tests__/components/Clock.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Clock from "../../components/Clock";
+import renderer from "react-test-renderer";
+
+var root = null;
+var clockRenderer = null;
+describe('<Clock />', () => {
+
+    describe('when given minutes and seconds (DOM)', () => {
+        beforeEach(() => {
+            root = document.createElement("div");
+            ReactDOM.render(
+                <Clock minutes={10} seconds={20} miliseconds={10} />, root
+            )
+        });
+        it('renders properly when given minutes and seconds', () => {
+            expect(root.childNodes[0].nodeName).toEqual("H2");
+            expect(root.childNodes[0].className).toMatch(/Clock/);
+            expect(root.childNodes[0].textContent).toMatch(/10:20/);
+        });
+
+        it('renders an h2 element', () => {
+            expect(root.childNodes[0].nodeName).toEqual("H2");
+        });
+
+        it('sets a Clock className', () => {
+            expect(root.childNodes[0].className).toMatch(/Clock/);
+        });
+
+        it('renders time properly', () => {
+            expect(root.childNodes[0].textContent).toMatch(/10:20/);
+        });
+
+    });
+
+    describe('when given minutes and seconds (TestRenderer)', () => {
+        beforeEach(() => {
+            clockRenderer = renderer.create(
+                <Clock minutes={10} seconds={20} miliseconds={10} />
+            )
+        });
+        it('renders properly', () => {
+            //expect(clockRenderer.toTree()).toEqual();
+            //console.log(clockRenderer.toJSON());
+            expect(clockRenderer.toJSON()).toMatchSnapshot();
+
+        });
+
+        it('renders an h2 element', () => {
+            expect(clockRenderer.toJSON().type).toEqual("h2");
+        });
+
+        it('sets a Clock className', () => {
+            expect(clockRenderer.toJSON().props).toMatchObject({ "className": expect.stringMatching(/Clock/) });
+        });
+
+        it('renders time properly', () => {
+            expect(root.childNodes[0].textContent).toMatch(/10:20/);
+            expect(clockRenderer.toJSON().children).toEqual(expect.arrayContaining(["10", "20"]));
+        });
+
+    });
+
+    it('renders properly when given minutes and seconds', () => {
+        expect(<Clock minutes={10} seconds={20} miliseconds={10} />).toEqual(<Clock minutes={10} seconds={20} miliseconds={10} />)
+    });
+});
