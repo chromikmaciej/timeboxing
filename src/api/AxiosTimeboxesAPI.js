f6f87c84c5c004872f94cac58a4b5ad7f5028e6d
Maciej Chromik <chromik.maciej@gmail.com>
2020-03-09 13:54:45 +0100

    axios complete

--- src/api/AxiosTimeboxesAPI.js
+++ src/api/AxiosTimeboxesAPI.js
@@ -3,24 +3,28 @@ import axios from "axios";
 const BASE_URL = "http://localhost:4000/timeboxes";
 const AxiosTimeboxesAPI = {
     getAllTimeboxes: async function getAllTimeboxes() {
-        const timeboxes = [];
+        const response = await axios.get(BASE_URL);
+        const timeboxes = response.data;
         return timeboxes;
     },
     addTimebox: async function addTimebox(timeboxToAdd) {
-        const addedTimebox = {};
+        const response = await axios.post(BASE_URL, timeboxToAdd);
+        const addedTimebox = response.data;
         return addedTimebox;
     },
     replaceTimebox: async function (timeboxToReplace) {
         if (!timeboxToReplace.id) {
             throw new Error("Timebox has to have an id to be updated");
         }
-        const replacedTimebox = {};
+        const response = await axios.put(`${BASE_URL}/${timeboxToReplace.id}`, timeboxToReplace);
+        const replacedTimebox = response.data
         return replacedTimebox;
     },
     removeTimebox: async function (timeboxToRemove) {
         if (!timeboxToRemove.id) {
             throw new Error("Timebox has to have an id to be updated");
         }
+        await axios.delete(`${BASE_URL}/${timeboxToRemove.id}`);
     }
 
 }
