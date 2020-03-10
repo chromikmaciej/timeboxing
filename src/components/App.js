8943734c469bbaf5e5070427b0cc453c0a5f1cf8
Maciej Chromik <chromik.maciej@gmail.com>
2020-03-10 13:54:06 +0100

    week 7 lesson 14 4:00

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,41 @@ import TimeboxList from "./TimeboxList";
 import EditableTimebox from "./EditableTimebox";
 import ErrorBoundary from "./ErrorBoundary";
 
+class App extends React.Component {
 
+    isUserLoggedIn() {
+        return true;
+    }
 
-function App() {
-    return (
-        <div className="App">
-            <ErrorBoundary message="Coś nie działa w całej aplikacji">
-                <TimeboxList />
-                <ErrorBoundary message="Coś nie działa w EditableTimebox">
-                    <EditableTimebox />
+    getUserEmail() {
+        return "al@example.com";
+    }
+
+    handleLogout = () => {
+        console.log("handle logout");
+    }
+    render() {
+        return (
+            <div className="App">
+                <ErrorBoundary message="Coś nie działa w całej aplikacji">
+                    {
+                        this.isUserLoggedIn() ?
+                        <>
+                            <header className="header">
+                                Witaj {this.getUserEmail()}
+                                <a onClick={this.handleLogout} className="header__logout-link" href="#">Wyloguj się</a>
+                            </header>
+                            <TimeboxList />
+                            <ErrorBoundary message="Coś nie działa w EditableTimebox">
+                                <EditableTimebox />
+                            </ErrorBoundary>
+                        </>:
+                        <div>Login form</div>
+                    }
                 </ErrorBoundary>
-            </ErrorBoundary>
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
 export default App;
