3b5d3688d43313377e4e8d1f72b14441d86161c5
Maciej Chromik <chromik.maciej@gmail.com>
2020-05-07 13:39:15 +0200

    week 11 lesson 8 10:30

--- src/components/TimeboxesList.js
+++ src/components/TimeboxesList.js
@@ -1,19 +1,10 @@
 import React, { useEffect } from "react";
 import { getAllTimeboxes } from "../reduceres";
-import { useStore } from "react-redux"; 
-import { useForceUpdate} from "./TimeboxesManager";
+import { connect } from "react-redux"; 
  
 export function TimeboxesList({ timeboxes, renderTimebox }) {
     return <div className="TimeboxesList">{timeboxes.map(renderTimebox)}</div>;
 }
 
-export function AllTimeboxesList({ renderTimebox }) {
-    const store = useStore();
-    const state = store.getState();
-    const forceUpdate = useForceUpdate();
-    useEffect(() => store.subscribe(forceUpdate), []);
-    return <TimeboxesList
-        timeboxes={getAllTimeboxes(state)}
-        renderTimebox={renderTimebox}
-    />
-}
+const mapStateToProps = (state) => ({ timeboxes: getAllTimeboxes(state)});
+export const AllTimeboxesList = connect(mapStateToProps)(TimeboxesList);
