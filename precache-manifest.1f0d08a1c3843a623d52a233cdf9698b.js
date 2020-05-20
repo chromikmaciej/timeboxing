700e9665f2c44cc85a5802f6605c07d0d48e764c
Maciej Chromik <chromik.maciej@gmail.com>
2020-05-20 22:10:25 +0200

    Updates

---
+++ precache-manifest.1f0d08a1c3843a623d52a233cdf9698b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba6f9819601b1928fc849ab62240f3f",
+    "url": "/timeboxing/index.html"
+  },
+  {
+    "revision": "43a13f4b7de763b753fb",
+    "url": "/timeboxing/static/css/main.2cdbe041.chunk.css"
+  },
+  {
+    "revision": "ee3252eb287b67a19554",
+    "url": "/timeboxing/static/js/2.2c176ae8.chunk.js"
+  },
+  {
+    "revision": "a866ea92209df983df36",
+    "url": "/timeboxing/static/js/3.846eba73.chunk.js"
+  },
+  {
+    "revision": "960f75a7531117c06c01",
+    "url": "/timeboxing/static/js/4.eefc9481.chunk.js"
+  },
+  {
+    "revision": "3c020ff62724ac21d681",
+    "url": "/timeboxing/static/js/5.a7c83f3c.chunk.js"
+  },
+  {
+    "revision": "43a13f4b7de763b753fb",
+    "url": "/timeboxing/static/js/main.5d3c9256.chunk.js"
+  },
+  {
+    "revision": "54dd21783ee1a2cbb49e",
+    "url": "/timeboxing/static/js/runtime~main.df1ccd57.js"
+  }
+]);
